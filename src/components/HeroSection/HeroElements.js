f7928f43cd457b76a65746827f191a0abbc567e5
Maulana Adam Sahid <62916459+masadamsahid@users.noreply.github.com>
2021-06-25 04:45:51 +0700

    adding the Encode Sans Expanded fonts and make the video bg darker

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -11,7 +11,18 @@ export const HeroContainer = styled.div`
   position: relative;
   z-index: 1;
   
-  /*Add before styled*/
+  :before{
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background:
+            linear-gradient(180deg, rgba(0,0,0,.2) 0%, rgba(0,0,0,.2) 100%),
+            linear-gradient(180deg, rgba(0,0,0,.2) 0%, transparent 100%);
+    z-index: 2;
+  }
 `
 
 export const HeroBg = styled.div`
