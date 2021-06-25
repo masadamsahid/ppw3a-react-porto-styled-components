b6102d032df1aba7ac08baa09dad8663b1ce9c12
Maulana Adam Sahid <62916459+masadamsahid@users.noreply.github.com>
2021-06-25 20:16:50 +0700

    Adding initial content of Contact Me page and making sure it's working with react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Navbar from "./components/Navbar";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages";
+import Contacts from "./pages/contacts";
 
 function App() {
     return (
         <Router>
-            <Home/>
+            <Switch>
+                <Route path={'/'} component={Home} exact/>
+                <Route path={'/'} component={Contacts}/>
+            </Switch>
         </Router>
     );
 }
---
+++ src/pages/contacts.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Contacts = () => {
+    return(
+        <div>
+            <h1>Contact Me Page</h1>
+        </div>
+    )
+}
+
+export default Contacts;
